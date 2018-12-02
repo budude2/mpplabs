0bd6073711ea30c4097bc5566f5a13064af53669
Unknown <mail@jboline.us>
2018-12-01 21:58:33 -0600

    Working messy generic code

---
+++ proj/fast_generic/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace boost::filesystem;
+
+namespace cv
+{
+    using std::vector;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cout << "Usage: tut3 path\n";
+        return 1;
+    }
+
+    unsigned int index = 0;
+    cv::vector<cv::Mat> img;
+    path p(argv[1]);
+
+    try
+    {
+        if (exists(p))
+        {
+            if (is_regular_file(p))
+                std::cout << "Please give a directory." << std::endl;
+
+            else if (is_directory(p))
+            {
+                std::cout << "Loading images from: " << p << "\n" << std::endl;
+
+                for (directory_entry &x : directory_iterator(p))
+                {
+                    if(is_regular_file(x.path()))
+                    {
+                        std::cout << "Loading: " << x.path().string() << std::endl;
+
+                        img.push_back(cv::imread(x.path().string()));
+
+                        if (!img[index].data)
+                        {
+                            std::cin.get();
+                            return -1;
+                        }
+
+                        index++;
+                    }
+                }
+            }
+            else
+            {
+                std::cout << p << " exists, but is not a regular file or directory" << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << p << " does not exist" << std::endl;
+        }
+    }
+
+    catch (const filesystem_error &ex)
+    {
+        std::cout << ex.what() << std::endl;
+    }
+
+    std::cout << "Images loaded..." << std::endl;
+    std::cout << "Vector size: " << img.size() << std::endl;
+
+    unsigned int width  = img[0].cols;
+    unsigned int height = img[0].rows;
+
+    cv::Mat res(height, width, CV_32FC3);
+
+    for(unsigned int i = 0; i < img.size(); i++)
+    {
+        img[i].convertTo(img[i], CV_32F);
+        res += img[i];
+
+    }
+
+    // for(unsigned int i = 0; i < img.size(); i++)
+    // {
+    //     res += img[i];
+    // }
+
+    res = res / img.size();
+
+    cv::vector<int> compression_param;
+    compression_param.push_back(cv::IMWRITE_JPEG_QUALITY);
+    compression_param.push_back(100);
+    res.convertTo(res, CV_8U);
+
+    cv::imwrite("result.jpg", res, compression_param);
+
+    return 0;
+}
