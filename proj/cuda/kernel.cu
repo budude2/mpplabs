fcf1aebe5c3fbc615c8dd97dd5f60169e53737f4
Jacob <mail@jboline.us>
2018-12-05 11:15:12 -0600

    Formatting

--- proj/cuda/kernel.cu
+++ proj/cuda/kernel.cu
@@ -8,9 +8,9 @@ __global__ void image_proc(unsigned char* images, unsigned char* output, unsigne
         const unsigned int image_idx  = yIndex * colorSize + (3 * xIndex);
         const unsigned int res_idx    = yIndex * colorSize + (3 * xIndex);
 
-        float blue_avg = 0;
+        float blue_avg  = 0;
         float green_avg = 0;
-        float red_avg = 0;
+        float red_avg   = 0;
         unsigned char blue_avg_char = 0;
         unsigned char green_avg_char = 0;
         unsigned char red_avg_char = 0;
@@ -22,9 +22,9 @@ __global__ void image_proc(unsigned char* images, unsigned char* output, unsigne
             red_avg   += images[imgNum * imageSize + image_idx + 2];
         }
 
-        blue_avg_char = blue_avg / numImg;
-        green_avg_char = green_avg / numImg;
-        red_avg_char = red_avg / numImg;
+        blue_avg_char  = blue_avg   / numImg;
+        green_avg_char = green_avg  / numImg;
+        red_avg_char   = red_avg    / numImg;
 
         output[res_idx]     = static_cast<unsigned char>(blue_avg_char);
         output[res_idx + 1] = static_cast<unsigned char>(green_avg_char);
