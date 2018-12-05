c58430c48b86e86f8da6aacbba417d6081d6bef8
Jacob <mail@jboline.us>
2018-12-05 11:52:33 -0600

    Get rid of ungeneralized fast version

--- proj/fast/main.cpp
+++ proj/fast/main.cpp
@@ -1,69 +1,46 @@
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include "support.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 2)
+    {
+        std::cout << "Usage: avg [path to images]\n";
+        return 1;
+    }
+
+    std::vector<cv::Mat> img;
     Timer timer;
 
-    std::cout << "Opening images......";
     startTime(&timer);
 
-    cv::Mat image1, image2, image3, image4, image5;
+    img = loadFiles(argv);
 
-    image1 = cv::imread("../noise_set2/noise1.jpg", 1);
-    image2 = cv::imread("../noise_set2/noise2.jpg", 1);
-    image3 = cv::imread("../noise_set2/noise3.jpg", 1);
-    image4 = cv::imread("../noise_set2/noise4.jpg", 1);
-    image5 = cv::imread("../noise_set2/noise5.jpg", 1);
-
-    if(!image1.data)
-    {
-        std::cout << "Could not open image 1" << std::endl;
-        return -1;
-    }
-    if(!image2.data)
-    {
-        std::cout << "Could not open image 2" << std::endl;
-        return -1;
-    }
-    if(!image3.data)
-    {
-        std::cout << "Could not open image 3" << std::endl;
-        return -1;
-    }
-    if(!image4.data)
-    {
-        std::cout << "Could not open image 4" << std::endl;
-        return -1;
-    }
-    if(!image5.data)
-    {
-        std::cout << "Could not open image 5" << std::endl;
-        return -1;
-    }
+    std::cout << "Images loaded..." << std::endl;
+    std::cout << "Vector size: " << img.size() << std::endl;
 
     stopTime(&timer);
+    std::cout << "Opening images......";
     std::cout << elapsedTime(timer) << " s" << std::endl;
 
     std::cout << "Processing images...";
     startTime(&timer);
 
-    image1.convertTo(image1, CV_32F);
-    image2.convertTo(image2, CV_32F);
-    image3.convertTo(image3, CV_32F);
-    image4.convertTo(image4, CV_32F);
-    image5.convertTo(image5, CV_32F);
+    unsigned int width  = img[0].cols;
+    unsigned int height = img[0].rows;
 
-    unsigned int width = image1.cols;
-    unsigned int height = image1.rows;
+    cv::Mat res(height, width, CV_32FC3);
 
-    cv::Mat res(height, width, CV_32F);
+    // Convert images to 3 channel float and find the sum.
+    for(unsigned int i = 0; i < img.size(); i++)
+    {
+        img[i].convertTo(img[i], CV_32F);
+        res += img[i];
+    }
 
-    res = (image1 + image2 + image3 + image4 + image5) / 5;
-
-    res.convertTo(res, CV_8U);
+    // Take the average
+    res = res / img.size();
 
     stopTime(&timer);
     std::cout << elapsedTime(timer) << " s" << std::endl;
@@ -71,20 +48,16 @@ int main()
     std::cout << "Writing image.......";
     startTime(&timer);
 
+    // Write the resulting image.
+    res.convertTo(res, CV_8U);
+
     std::vector<int> compression_param;
     compression_param.push_back(cv::IMWRITE_JPEG_QUALITY);
     compression_param.push_back(100);
-
     cv::imwrite("result.jpg", res, compression_param);
 
     stopTime(&timer);
     std::cout << elapsedTime(timer) << " s" << std::endl;
 
-    image1.release();
-    image2.release();
-    image3.release();
-    image4.release();
-    image5.release();
-    res.release();
     return 0;
 }
--- proj/fast/support.cpp
+++ proj/fast/support.cpp
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <boost/filesystem.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include "support.h"
 
 void startTime(Timer* timer)
@@ -16,3 +18,60 @@ float elapsedTime(Timer timer)
         return ((float) ((timer.endTime.tv_sec - timer.startTime.tv_sec) + (timer.endTime.tv_usec - timer.startTime.tv_usec)/1.0e6));
 }
 
+std::vector<cv::Mat> loadFiles(char **argv)
+{
+
+    std::vector<cv::Mat> img;
+    unsigned int index = 0;
+
+    boost::filesystem::path p(argv[1]);
+
+    // Load all images from the specified folder
+    try
+    {
+        if (exists(p))
+        {
+            if (is_regular_file(p))
+            {
+                std::cout << "Please give a directory." << std::endl;
+            }
+            else if (is_directory(p))
+            {
+                std::cout << "Loading images from: " << p << "\n" << std::endl;
+
+                for (boost::filesystem::directory_entry &x : boost::filesystem::directory_iterator(p))
+                {
+                    if(is_regular_file(x.path()))
+                    {
+                        std::cout << "Loading: " << x.path().string() << std::endl;
+
+                        img.push_back(cv::imread(x.path().string()));
+
+                        if (!img[index].data)
+                        {
+                            std::cin.get();
+                            //return -1;
+                        }
+
+                        index++;
+                    }
+                }
+            }
+            else
+            {
+                std::cout << p << " exists, but is not a regular file or directory" << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << p << " does not exist" << std::endl;
+        }
+    }
+
+    catch (const boost::filesystem::filesystem_error &ex)
+    {
+        std::cout << ex.what() << std::endl;
+    }
+
+    return img;
+}
