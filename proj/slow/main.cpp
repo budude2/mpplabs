af2c89fc6a8627b7c401c1780442d0089efdf3a5
Jacob <mail@jboline.us>
2018-12-05 11:51:09 -0600

    Make slow algorithm generic

--- proj/slow/main.cpp
+++ proj/slow/main.cpp
@@ -1,87 +1,59 @@
 #include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include "support.h"
 
-int main()
+int main(int argc, char* argv[])
 {
     Timer timer;
+    std::vector<cv::Mat> img;
 
-    std::cout << "Opening images......";
+    //std::cout << "Opening images......";
     startTime(&timer);
-    
-    cv::Mat image1, image2, image3, image4, image5;
 
-    image1 = cv::imread("../noise_set2/noise1.jpg", 1);
-    image2 = cv::imread("../noise_set2/noise2.jpg", 1);
-    image3 = cv::imread("../noise_set2/noise3.jpg", 1);
-    image4 = cv::imread("../noise_set2/noise4.jpg", 1);
-    image5 = cv::imread("../noise_set2/noise5.jpg", 1);
+    img = loadFiles(argv);
 
-    if(!image1.data)
-    {
-        std::cout << "Could not open image 1" << std::endl;
-        return -1;
-    }
-    if(!image2.data)
-    {
-        std::cout << "Could not open image 2" << std::endl;
-        return -1;
-    }
-    if(!image3.data)
-    {
-        std::cout << "Could not open image 3" << std::endl;
-        return -1;
-    }
-    if(!image4.data)
-    {
-        std::cout << "Could not open image 4" << std::endl;
-        return -1;
-    }
-    if(!image5.data)
-    {
-        std::cout << "Could not open image 5" << std::endl;
-        return -1;
-    }
+    std::cout << "Images loaded..." << std::endl;
+    std::cout << "Vector size: " << img.size() << std::endl;
 
     stopTime(&timer);
+    std::cout << "Opening images......";
     std::cout << elapsedTime(timer) << " s" << std::endl;
 
     std::cout << "Processing images...";
     startTime(&timer);
 
-    unsigned int width = image1.cols;
-    unsigned int height = image1.rows;
+    unsigned int width  = img[0].cols;
+    unsigned int height = img[0].rows;
+    unsigned int numImages = img.size();
 
-    cv::Mat res(height, width, CV_32FC3);
+    cv::Mat res(height, width, CV_8UC3);
 
-    image1.convertTo(image1, CV_32F);
-    image2.convertTo(image2, CV_32F);
-    image3.convertTo(image3, CV_32F);
-    image4.convertTo(image4, CV_32F);
-    image5.convertTo(image5, CV_32F);
+    unsigned long long blueAvg  = 0;
+    unsigned long long greenAvg = 0;
+    unsigned long long redAvg   = 0;
 
     for(unsigned int col = 0; col < width; col++)
     {
        for(unsigned int row = 0; row < height; row++)
        {
-            cv::Vec3f pixel0 = image1.at<cv::Vec3f>(row, col);
-            cv::Vec3f pixel1 = image2.at<cv::Vec3f>(row, col);
-            cv::Vec3f pixel2 = image3.at<cv::Vec3f>(row, col);
-            cv::Vec3f pixel3 = image4.at<cv::Vec3f>(row, col);
-            cv::Vec3f pixel4 = image5.at<cv::Vec3f>(row, col);
+            blueAvg  = 0;
+            greenAvg = 0;
+            redAvg   = 0;
 
-            float blueAvg  = (pixel0.val[0] + pixel1.val[0] + pixel2.val[0] + pixel3.val[0] + pixel4.val[0]) / 5;
-            float greenAvg = (pixel0.val[1] + pixel1.val[1] + pixel2.val[1] + pixel3.val[1] + pixel4.val[1]) / 5;
-            float redAvg   = (pixel0.val[2] + pixel1.val[2] + pixel2.val[2] + pixel3.val[2] + pixel4.val[2]) / 5;
+            for(unsigned int imgNum = 0; imgNum < numImages; imgNum++)
+            {
+                cv::Vec3b pixel = img[imgNum].at<cv::Vec3b>(row, col);
 
-            res.at<cv::Vec3f>(row, col)[0] = blueAvg;
-            res.at<cv::Vec3f>(row, col)[1] = greenAvg;
-            res.at<cv::Vec3f>(row, col)[2] = redAvg;
+                blueAvg  += pixel.val[0];
+                greenAvg += pixel.val[1];
+                redAvg   += pixel.val[2];
+
+                res.at<cv::Vec3b>(row, col)[0] = static_cast<unsigned char>(blueAvg  / numImages);
+                res.at<cv::Vec3b>(row, col)[1] = static_cast<unsigned char>(greenAvg / numImages);
+                res.at<cv::Vec3b>(row, col)[2] = static_cast<unsigned char>(redAvg   / numImages);
+            }
        }
     }
-
-    res.convertTo(res, CV_8U);
 
     stopTime(&timer);
     std::cout << elapsedTime(timer) << " s" << std::endl;
@@ -98,11 +70,6 @@ int main()
     stopTime(&timer);
     std::cout << elapsedTime(timer) << " s" << std::endl;
 
-    image1.release();
-    image2.release();
-    image3.release();
-    image4.release();
-    image5.release();
     res.release();
 
     return 0;
--- proj/slow/support.h
+++ proj/slow/support.h
@@ -2,19 +2,22 @@
 #define __FILEH__
 
 #include <sys/time.h>
+#include <opencv2/opencv.hpp>
+
 
 typedef struct
 {
         struct timeval startTime;
-            struct timeval endTime;
+        struct timeval endTime;
 } Timer;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-        void startTime(Timer* timer);
-            void stopTime(Timer* timer);
-                float elapsedTime(Timer timer);
+    void startTime(Timer* timer);
+    void stopTime(Timer* timer);
+    float elapsedTime(Timer timer);
+    std::vector<cv::Mat> loadFiles(char **argv);
 #ifdef __cplusplus
 }
 #endif
